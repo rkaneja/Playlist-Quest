fc97c29668a3b377710cef6d878bc735f618adf7
rkaneja <rahulaneja1121@gmail.com>
2024-02-19 00:41:29 -0500

    file cleanup

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("loginBtn").addEventListener("click", () => {
-    const clientId = '3f7e3ab2ed184531811ffd9eb4db0c4c';
-    const redirectUri = "https://rkaneja.github.io/Playlist-Quest/callback.html";
-    const scopes = encodeURIComponent("user-top-read playlist-modify-private");
-
-    const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${scopes}&redirect_uri=${encodeURIComponent(redirectUri)}`;
-    window.location.href = authUrl;
-});
